e41dd9190ff8cb3f5f14683b2cf5db064ebb83ad
Matheus Aprigio <matheusaprig@gmail.com>
2022-04-06 14:46:40 -0300

    Adicionado delecao de classes de forma customizada

--- course/course/src/main/java/com/ead/course/models/CourseModel.java
+++ course/course/src/main/java/com/ead/course/models/CourseModel.java
@@ -6,11 +6,12 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
+import org.hibernate.annotations.*;
 
 import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.Set;
