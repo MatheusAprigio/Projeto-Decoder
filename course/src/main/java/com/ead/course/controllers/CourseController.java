be7595414695e1aadaff81ae4fb45600dec53de7
Matheus Aprigio <matheusaprig@gmail.com>
2022-05-07 22:33:24 -0300

    Adicionado validacao customizada User Instructor

--- course/src/main/java/com/ead/course/controllers/CourseController.java
+++ course/src/main/java/com/ead/course/controllers/CourseController.java
@@ -4,6 +4,8 @@ import com.ead.course.dtos.CourseDto;
 import com.ead.course.models.CourseModel;
 import com.ead.course.services.CourseService;
 import com.ead.course.specifications.SpecificationTemplate;
+import com.ead.course.validation.CourseValidator;
+import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,6 +14,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -29,8 +32,18 @@ public class CourseController {
     @Autowired
     CourseService courseService;
 
+    @Autowired
+    CourseValidator courseValidator;
+
     @PostMapping
-    public ResponseEntity<Object> saveCourse(@RequestBody @Valid CourseDto courseDto){
+    public ResponseEntity<Object> saveCourse(@RequestBody CourseDto courseDto, Errors errors){
+
+        courseValidator.validate(courseDto, errors);
+
+        if (errors.hasErrors()){
+           return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors.getAllErrors());
+        }
+
         var courseModel = new CourseModel();
         BeanUtils.copyProperties(courseDto, courseModel);
 
