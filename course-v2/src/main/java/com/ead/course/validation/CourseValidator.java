c3c52e80e99fd502c69d6c151f7740b941c348df
Matheus Aprigio <matheusaprig@gmail.com>
2022-06-08 14:02:42 -0300

    Alterado regra de autorizacao para INSTRUCTOR

--- course-v2/src/main/java/com/ead/course/validation/CourseValidator.java
+++ course-v2/src/main/java/com/ead/course/validation/CourseValidator.java
@@ -1,12 +1,14 @@
 package com.ead.course.validation;
 
 
+import com.ead.course.configs.security.AuthenticationCurrentUserService;
 import com.ead.course.dtos.CourseDto;
 import com.ead.course.enums.UserType;
 import com.ead.course.models.UserModel;
 import com.ead.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
@@ -24,6 +26,9 @@ public class CourseValidator implements Validator {
     @Autowired
     UserService userService;
 
+    @Autowired
+    AuthenticationCurrentUserService authCurrentUserService;
+
     @Override
     public boolean supports(Class<?> aClass) {
         return false;
@@ -39,12 +44,17 @@ public class CourseValidator implements Validator {
     }
 
     private void validateUserInstructor(UUID userInstructor, Errors errors){
-        Optional<UserModel> userModelOptional = userService.findById(userInstructor);
-        if(!userModelOptional.isPresent()){
-            errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found.");
-        }
-        if(userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())){
-            errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
+        UUID currentUserId = authCurrentUserService.getCurrentUser().getUserId();
+        if (currentUserId.equals(userInstructor)){
+            Optional<UserModel> userModelOptional = userService.findById(userInstructor);
+            if(!userModelOptional.isPresent()){
+                errors.rejectValue("userInstructor", "UserInstructorError", "Instructor not found.");
+            }
+            if(userModelOptional.get().getUserType().equals(UserType.STUDENT.toString())){
+                errors.rejectValue("userInstructor", "UserInstructorError", "User must be INSTRUCTOR or ADMIN.");
+            }
+        }else {
+            throw new AccessDeniedException("Forbidden");
         }
     }
 }
